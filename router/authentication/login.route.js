9db6e4e1dbc40ab56551d714d7c3e7028b9ab381
prabhat328 <prabhat328@outlook.com>
2024-07-19 15:38:04 +0530

    set route handler of auth related functionalities to a single file. Changed API route name to /api/... Deleted other route files for auth as not required now

--- router/authentication/login.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { loginUser } from "../../controller/authentication/login.controller.js"
-
-const router = express.Router();
-
-router.post('/', loginUser);
-
-export default router;
--- router/authentication/passwordReset.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { resetPassword } from "../../controller/authentication/passwordReset.controller.js";
-
-const router = express.Router();
-
-router.post('/', resetPassword);
-
-export default router;
