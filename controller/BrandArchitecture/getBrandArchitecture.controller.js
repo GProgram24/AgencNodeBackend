27cd4bce8a21137eb4084f5662c4fb1f357fd500
prabhat328 <prabhat328@outlook.com>
2024-07-23 18:59:20 +0530

    modified code as per response required by programming on json object, removed unwanted blank arrays and unnecessary fields from the response object.

--- controller/BrandArchitecture/getBrandArchitecture.controller.js
+++ controller/BrandArchitecture/getBrandArchitecture.controller.js
@@ -4,43 +4,58 @@ import Category from "../../model/Brand/category.model.js";
 import SubCategory from "../../model/Brand/subCategory.model.js";
 import ProductService from "../../model/Brand/productService.model.js";
 import mongoose from "mongoose";
+import cleanBrandArchitecture from "./cleanBrandArchitecture.controller.js";
 
 export const fetchBrandHierarchy = async (req, res) => {
     try {
         const brandDetails = await Brand.findOne({ name: req.body.brand });
         const brandId = brandDetails._id;
         // Fetch the main brand
-        const brand = await Brand.findById(brandId)
+        const brandArchitecture = await Brand.findById(brandId)
+            .select("-_id -createdAt -updatedAt -__v -managedBy -accountId -_id")
             .populate({
                 path: 'subBrands',
+                select: "-_id -createdAt -updatedAt -__v",
                 populate: {
                     path: 'categories',
+                    select: "-_id -createdAt -updatedAt -__v",
                     populate: {
                         path: 'subCategories',
+                        select: "-_id -createdAt -updatedAt -__v",
                         populate: {
-                            path: 'products'
+                            path: 'products',
+                            select: "-createdAt -updatedAt -subCategory -category -subBrand -brand -__v"
                         }
                     }
                 }
             })
             .populate({
                 path: 'categories',
+                select: "-_id -createdAt -updatedAt -__v",
                 populate: {
                     path: 'subCategories',
+                    select: "-_id -createdAt -updatedAt -__v",
                     populate: {
-                        path: 'products'
+                        path: 'products',
+                        select: "-createdAt -updatedAt -subCategory -category -subBrand -brand -__v"
                     }
                 }
             })
             .populate({
                 path: 'subCategories',
+                select: "-_id -createdAt -updatedAt -__v",
                 populate: {
-                    path: 'products'
+                    path: 'products',
+                    select: "-createdAt -updatedAt -subCategory -category -subBrand -brand -__v"
                 }
             })
-            .populate('products');
+            .populate({
+                path: "products",
+                select: "-createdAt -updatedAt -subCategory -category -subBrand -brand -__v"
+            });
         // need to clean the empty arrays in object obtained
-        return res.status(200).json(brand);
+        const cleanedArchitecture = cleanBrandArchitecture(brandArchitecture)
+        return res.status(200).json({ brand: [cleanedArchitecture] });
     } catch (error) {
         console.error('Error fetching brand hierarchy:', error);
         return res.status(500).json({ message: "Internal server error" });
