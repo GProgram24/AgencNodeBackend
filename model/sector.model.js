87b28df530aee08cc4a27608a7f5d738305841d8
prabhat328 <prabhat328@outlook.com>
2024-07-23 19:19:27 +0530

    modified schema as required. Added product reference, array for subSector, used enum since possible sectors are predefined

--- model/sector.model.js
+++ model/sector.model.js
@@ -1,11 +1,22 @@
 import mongoose from "mongoose";
 
 const sectorSchema = new mongoose.Schema({
-  id: { type: Number, required: true, unique: true },
-  name: { type: String, required: true },
-  userId: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now },
-  createdBy: { type: Number, required: true },
-});
+  productServiceId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "ProductService" },
+  name: {
+    type: String,
+    required: true,
+    enum: ["Technology",
+      "Consumer Goods",
+      "Healthcare",
+      "Food and Beverage",
+      "Entertainment and Media",
+      "Automotive",
+      "Finance and Banking",
+      "Education"]
+  },
+  subSector: [{ type: String, required: true }],
+},
+  { timestamps: true }
+);
 
-export default mongoose.model("Sector ", sectorSchema);
+export default mongoose.model("Sector", sectorSchema);
