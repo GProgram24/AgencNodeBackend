6c40a272309cd2e023e240a342881268ec4bae0f
prabhat328 <prabhat328@outlook.com>
2024-07-23 19:09:44 +0530

    modified schema as required with product ref

--- model/productServiceMeta.model.js
+++ model/productServiceMeta.model.js
@@ -1,15 +1,13 @@
 import mongoose from "mongoose";
 
 const productServiceMetaSchema = new mongoose.Schema({
-  id: { type: Number, required: true, unique: true },
-  productService: { type: Number, required: true },
+  productServiceId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "ProductService" },
   description: { type: String, required: true },
   feature: { type: String, required: true },
   attributes: { type: String, required: true },
-  usp: { type: String, required: true },
-  userId: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now },
-  createdBy: { type: Number, required: true },
-});
+  usp: { type: String, required: true }
+},
+  { timestamps: true }
+);
 
 export default mongoose.model("ProductServiceMeta", productServiceMetaSchema);
