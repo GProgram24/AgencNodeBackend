6c1524f6cdbb8be1112927510b7ebce3c01ba8a1
prabhat328 <prabhat328@outlook.com>
2024-07-25 15:46:34 +0530

    Changed route name as per conventions

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ app.get("/", (req, res) => {
   res.json({ status: "running" });
 });
 app.use("/api/auth", authRouter);
-app.use("/api/assign-creator", setCreator);
-app.use("/api/assign-custodian", setCustodian);
+app.use("/api/creator", setCreator);
+app.use("/api/custodian", setCustodian);
 app.use("/api/assign-role", setEditorViewer);
 app.use("/api/check", checkAvailability);
 app.use("/api/brand", brandHierarchy);
