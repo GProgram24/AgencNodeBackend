950c041eb3337c905e26509a882e4fa52e6bb048
prabhat328 <prabhat328@outlook.com>
2024-07-29 19:10:14 +0530

    added function to get available platforms from database

--- controller/platform/addPlatform.controller.js
+++ controller/platform/addPlatform.controller.js
@@ -1,5 +1,19 @@
 import oAuthClientCredentialsModel from "../../model/platform/oAuthClientCredentials.model.js"
 
+// Get available platforms from database
+export const getPlatform = async (req, res) => {
+    try {
+        // fetch all platforms from database and send the names in response
+        const getPlatforms = await oAuthClientCredentialsModel.find();
+        const platforms = getPlatforms.map(item => item.platform);
+        return res.status(200).json({ message: platforms });
+    } catch (error) {
+        console.log("Error at fetching platforms:", error);
+        return res.status(500).json({ message: "Server error" });
+    }
+}
+
+// Add platform to database
 export const addPlatform = async (req, res) => {
     try {
         if (req.body.devToken == "prabhaT2608") {
