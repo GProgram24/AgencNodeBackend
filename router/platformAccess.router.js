84d1ba4f26b0938aee3df162b91acad21fb35bbf
prabhat328 <prabhat328@outlook.com>
2024-07-29 19:02:59 +0530

    added route for getting available platforms, standardized function name

--- router/platformAccess.router.js
+++ router/platformAccess.router.js
@@ -1,16 +1,16 @@
 import express from "express";
-import { getAccess1, getAccess2 } from "../controller/platform/platformAccess.controller.js";
+import { platformResponse } from "../controller/platform/platformAccess.controller.js";
 import { getUrl } from "../controller/platform/getPlatformUrl.controller.js";
-import { addPlatform } from "../controller/platform/addPlatform.controller.js";
+import { getPlatform, addPlatform } from "../controller/platform/addPlatform.controller.js";
 
 const router = express.Router();
 
-router.post("/access", getAccess1); // redirect urls on platform
-router.get("/access", getAccess2); // redirect urls on platform
+router.get("/available", getPlatform); // To send available platforms to frontend
+router.get("/access", platformResponse); // redirect urls specified on platforms
 router.get("/url", getUrl); // To create url for accessing platforms
-router.post("/add", addPlatform); // To add developer data for accessing platforms
+router.post("/add", addPlatform); // To add developer data for accessing platforms. Only for development purpose
 router.post("/:others", (req, res) => {
-  res.status(422).json({ message: "Invalid request" });
+  res.status(422).json({ message: "Invalid url" });
 });
 
 export default router;
