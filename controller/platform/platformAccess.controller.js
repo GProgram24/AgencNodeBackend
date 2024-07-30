d95138184e4ffae199615aa3c5ce0e7aa127ba2c
prabhat328 <prabhat328@outlook.com>
2024-07-30 16:09:33 +0530

    changed redirect google router to function for all platform redirects

--- controller/platform/platformAccess.controller.js
+++ controller/platform/platformAccess.controller.js
@@ -2,16 +2,20 @@ import oAuthClientCredentials from "../../model/platform/oAuthClientCredentials.
 import { googleRedirect } from "./google.controller.js";
 
 // For Google Ads
-export const googleResponse = async (req, res) => {
+export const platformResponse = async (req, res) => {
     // Identify platform response is coming from // Or make separate route for platforms
     // Fetch ids from database
     // get state from the url and pass
-    try {
-        const clientDetail = await oAuthClientCredentials.findOne({ platform: "googleAds" });
-        const state = "testing";
-        await googleRedirect(req, res, clientDetail.clientId, clientDetail.clientSecret, clientDetail.redirectURL, state);
-    } catch (error) {
-        console.log("Error at google ads platform access:", error);
-        res.status(500).json({ message: "Server error" });
+    if (req.params.platformName == "googleads") {
+        try {
+            const clientDetail = await oAuthClientCredentials.findOne({ platform: "googleAds" });
+            const state = "testing";
+            await googleRedirect(req, res, clientDetail.clientId, clientDetail.clientSecret, clientDetail.redirectURL, state);
+        } catch (error) {
+            console.log("Error at google ads platform access:", error);
+            res.status(500).json({ message: "Server error" });
+        }
+    } else {
+        res.json({ message: "Not available yet" });
     }
 }
--- router/platformAccess.router.js
+++ router/platformAccess.router.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { googleResponse } from "../controller/platform/platformAccess.controller.js";
+import { platformResponse } from "../controller/platform/platformAccess.controller.js";
 import { getUrl } from "../controller/platform/getPlatformUrl.controller.js";
 import { getPlatform, addPlatform } from "../controller/platform/addPlatform.controller.js";
 
 const router = express.Router();
 
 router.get("/available", getPlatform); // To send available platforms to frontend
-router.get("/access", googleResponse); // redirect urls specified on google ads, to change route name
+router.get("/access/:platformName", platformResponse); // redirect url specified on platforms
 router.get("/url", getUrl); // To create url for accessing platforms
 router.post("/add", addPlatform); // To add developer data for accessing platforms. Only for development purpose
 router.post("/:others", (req, res) => {
