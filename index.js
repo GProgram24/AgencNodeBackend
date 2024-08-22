75431a6b7d8a73e5d6fb4a54552e5de59db86ef9
unknown <prabhat@your-agenc.com>
2024-08-22 15:24:05 +0530

    removed self signed verif

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import logger from "morgan";
 import http from "http";
-import https from "https"; // Uncomment if using SSL
-import fs from "fs"; // Required if using SSL
 
 import authRouter from "./router/authentication/auth.route.js";
 import setCreator from "./router/BrandArchitecture/setupCreator.route.js";
@@ -27,42 +25,32 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-// Uncomment the following lines if using HTTPS with a self-signed certificate
-const options = {
-  key: fs.readFileSync("./config/key.pem"),
-  cert: fs.readFileSync("./config/cert.pem"),
-};
-
-// HTTPS server
-const server = https.createServer(options, app);
-
-
 // HTTP server created from the Express app
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
 // CORS configuration
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://agenc-frontend.vercel.app",
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://agenc-frontend.vercel.app",
+// ];
 
-// Middleware
+// // Middleware
 app.use(bodyParser.json());
 app.use(logger("dev"));
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // Allow requests with no origin (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         const msg =
+//           "The CORS policy for this site does not allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 // To check server status
 app.get("/", (req, res) => {
@@ -92,10 +80,10 @@ mongoose
     console.log("Connected to MongoDB");
     // to keep free server up
     // Note: uncomment below code only when pushing on server, do not use on localhost
-    setInterval(async () => {
-        const response = await fetch("https://agencnodebackend.onrender.com/");
-        console.log(await response.json());
-    }, 600000)
+    // setInterval(async () => {
+    //     const response = await fetch("https://agencnodebackend.onrender.com/");
+    //     console.log(await response.json());
+    // }, 600000)
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
