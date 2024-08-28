ee9b98be0016643a2179a1db804bdf15d1888b64
unknown <prabhat@your-agenc.com>
2024-08-28 16:39:42 +0530

    changed reference from productServiceMeta to ProductService

--- model/Project/task.model.js
+++ model/Project/task.model.js
@@ -34,7 +34,7 @@ const taskSchema = new mongoose.Schema(
     },
     product: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "ProductServiceMeta",
+      ref: "ProductService",
       required: true,
     },
     targetAudience: {
