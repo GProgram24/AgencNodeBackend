776f1a4d7d9e0fb5572af0a8c7b57b5fb606599e
Prabhat <prabhat@your-agenc.com>
2024-09-25 10:41:16 +0530

    corrected imports for websocket router file, misconfured in merge from main branch

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ import projectRoute from "./router/Project/project.route.js";
 import { updateOnboardingProgress } from "./controller/misc/onboardingUpdate.function.js";
 import fastAPIHandler from "./router/fastapiHandler.router.js";
 import { getSampleTestingTask } from "./controller/misc/sampleTestingTask.function.js";
-import { setupWebSockets } from "./controller/fastAPI/webSocketHandler.js";
+import websocketRoutes from "./router/WebSocket/websocket.route.js";
 
 dotenv.config();
 
@@ -78,7 +78,7 @@ app.use("/api/projects", projectContentRoute);
 // to update onboarding progress, keep as last route
 app.patch("/api/onboarding/progress", updateOnboardingProgress);
 // web-socket
-setupWebSockets(server);
+websocketRoutes(io);
 
 
 // MongoDB Connection
